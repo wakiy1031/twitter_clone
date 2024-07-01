537fa0adaf8bf5d6cfea802924cd69f33c8e53c9
wakiy1031 <wakiy1031@gmail.com>
2024-07-01 10:54:14 +0900

    githubログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable, :trackable
+         :confirmable, :lockable, :timeoutable, :trackable,
+         :omniauthable, omniauth_providers: %i[github]
 
-  with_options presence: true do
-    validates :phone
-    validates :birthdate
-    validates :user_name, uniqueness: true
+  validates :phone, presence: true, unless: :provider_github?
+  validates :birthdate, presence: true, unless: :provider_github?
+  validates :user_name, presence: true, uniqueness: true
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.user_name = auth.info.nickname
+      user.skip_confirmation!
+    end
+  end
+
+  private
+
+  def provider_github?
+    provider == 'github'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   resources :posts, only: %i[index]
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
 
   root 'posts#index'
 end
