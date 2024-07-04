bfd2f4665ee39d1a412ae42741da15937a4965f3
wakiy1031 <wakiy1031@gmail.com>
2024-07-04 09:12:37 +0900

    seed作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,7 @@
 class PostsController < ApplicationController
   def index
     @user = user_signed_in? ? current_user : User.new
+
+    @posts = Post.includes(:user).latest
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+10.times do |u|
+  user = User.find_or_initialize_by(
+    user_name: "テストユーザー#{u + 1}",
+    email: "test-#{u + 1}@example.com",
+    phone: "0901111222#{u + 1}",
+  )
+  user.birthdate = "2024-07-03"
+  user.password = "password"
+  user.password_confirmation = "password"
+  user.skip_confirmation!
+  user.save!
+
+  5.times do |t|
+    post = user.posts.build(
+      content: "テストユーザー#{u + 1}の#{t + 1}個目のツイート"
+    )
+    post.save!
+  end
+
+end
+
+puts "ユーザーと投稿の確認:"
+User.all.each do |user|
+  puts "ユーザー: #{user.user_name}"
+  user.posts.each do |post|
+    puts "  - #{post.content}"
+  end
+  puts "\n"
+end
