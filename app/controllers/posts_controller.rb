b3ae084f4bca288ee8549ceeaf6357b97957bf1d
wakiy1031 <wakiy1031@gmail.com>
2024-07-04 09:31:05 +0900

    post一覧表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,6 @@ class PostsController < ApplicationController
   def index
     @user = user_signed_in? ? current_user : User.new
 
-    @posts = Post.includes(:user).latest
+    @posts = Post.includes(:user).order(created_at: :desc)
   end
 end
