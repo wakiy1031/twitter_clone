c55b8390454cde7bc00225f293406b7a2cf313b7
wakiy1031 <wakiy1031@gmail.com>
2024-07-05 12:08:28 +0900

    ページネーション設置

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,12 @@ class PostsController < ApplicationController
   def index
     @user = user_signed_in? ? current_user : User.new
 
-    @posts = Post.includes(:user).order(created_at: :desc)
-    @followees_posts = Post.includes(:user).where(user_id: current_user.followee_ids).order(created_at: :desc)
+    @posts = Post.includes(:user).order(created_at: :desc).page(params[:posts_page]).per(10)
+    @followees_posts = Post.includes(:user)
+                           .where(user_id: current_user.followee_ids)
+                           .order(created_at: :desc)
+                           .page(params[:followees_page]).per(10)
+
+    @active_tab = params[:tab] || 'for-you'
   end
 end
