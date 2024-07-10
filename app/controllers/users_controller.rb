61254fa78f69fa81ab674bd1919a2bdce53fb3cb
wakiy1031 <wakiy1031@gmail.com>
2024-07-10 12:44:04 +0900

    sidebarのpostボタンからも投稿

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @post = Post.new
     @posts = @user.posts.includes_desc.page(params[:posts_page]).per(10)
     @like_posts = @user.like_posts.includes_desc.page(params[:like_page]).per(10)
     @repost_posts = @user.repost_posts.includes_desc.page(params[:repost_page]).per(10)
