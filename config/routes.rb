2e6b746fd222718a22fea538abb7e95325e566f2
wakiy1031 <wakiy1031@gmail.com>
2024-07-13 11:20:17 +0900

    いいね作成と削除処理を追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   }
   resources :users, only: %i[show edit update]
   resources :posts, only: %i[index show create] do
-    resources :comments, only: [:create]
+    resources :comments, only: %i[create]
+    resource :likes, only: %i[create destroy]
   end
 
   root 'posts#index'
