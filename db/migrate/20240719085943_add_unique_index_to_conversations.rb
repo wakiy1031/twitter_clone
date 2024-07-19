64dd2dac30f0d189b9888d7a6cdd748e88d2224a
wakiy1031 <wakiy1031@gmail.com>
2024-07-19 18:05:43 +0900

    rubocopを通す

--- db/migrate/20240719085943_add_unique_index_to_conversations.rb
+++ db/migrate/20240719085943_add_unique_index_to_conversations.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class AddUniqueIndexToConversations < ActiveRecord::Migration[7.0]
   def change
-    add_index :conversations, [:user_id, :room_id], unique: true
+    add_index :conversations, %i[user_id room_id], unique: true
   end
 end
