e07d1db1a9302b02e71c9eda53e10ea081c197eb
wakiy1031 <wakiy1031@gmail.com>
2024-07-25 07:21:13 +0900

    userモデルの単体テスト

---
+++ test/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    sequence(:user_name) { |n| "test_user#{n}" }
+    sequence(:email) { |n| "test-#{n}@exmaple.com" }
+    phone { '09012345678' }
+    birthdate { '2024-07-03' }
+    password { 'password' }
+    password_confirmation { 'password' }
+  end
+end
